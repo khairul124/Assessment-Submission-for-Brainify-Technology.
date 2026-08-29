29092027f55f903334ed11800fec51a960446bab
ninetysixbd-source <ninetysixbd@gmail.com>
2026-08-29 23:09:39 +0600

    Add scoring.py, duplication.py, update parser.py and README

---
+++ signal-sifter-takehome/scoring.py
@@ -0,0 +1,10 @@
+from datetime import date 
+Refference_date=date(2026,9,1)
+def calculate_score(product):
+    age_days=(Refference_date-product.last_updated).days
+    if age_days>90:
+        return 0
+    installs_point=min(product.installs/1000,50)
+    rating_points = (product.ratting - 3.0) * 10
+    review_points = min(product.review_count / 100, 20)
+    return round(installs_points + rating_points+ review_points,2,)
